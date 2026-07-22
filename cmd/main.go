7dca667b60de9f97dba5daae96f1c4e91ef02903
Jonas Ababio <93048580+mk-milly02@users.noreply.github.com>
2026-07-22 19:51:34 +0000

    Refactor main function for improved IP address handling and server initialization

--- cmd/main.go
+++ cmd/main.go
@@ -7,81 +7,61 @@ import (
 	"log"
 	"net"
 	"north-polaris/internal/forwarder"
+	"north-polaris/internal/server"
 	"strconv"
+	"time"
 )
 
-const DEFAULT_LOCAL_NAME_SERVER = "127.0.0.1"
+const DEFAULT_LOCAL_NAME_SERVER = "127.0.0.100"
 const DEFAULT_PORT = 53
 
+var ipAddress = flag.String("ip", DEFAULT_LOCAL_NAME_SERVER, "specified ip address for incomming requests")
 var port = flag.Int("p", DEFAULT_PORT, "specified port for incomming requests")
-var debug = flag.Bool("d", false, "enable debug mode")
+var debug = flag.Bool("d", false, "print hex dumps")
 
 func main() {
 	flag.Parse()
 
-	if *port != 53 && *port <= 1023 {
-		log.Fatal("use ephimeral ports")
+	if *ipAddress == "" {
+		log.Fatal("ip address is required")
+	}
+	if net.ParseIP(*ipAddress) == nil {
+		log.Fatal("invalid ip address")
+	}
+	if *port < 1 || *port > 65535 {
+		log.Fatal("invalid port number")
 	}
 
-	serverAddr, err := net.ResolveUDPAddr("udp", DEFAULT_LOCAL_NAME_SERVER+":"+strconv.Itoa(*port))
+	c := server.NewCache(5*time.Minute, 1*time.Minute)
+	s := server.NewServer(net.IPAddr{IP: net.ParseIP(*ipAddress)}, *port, "udp")
+	connection, err := s.Run()
 	if err != nil {
-		log.Fatal("could to resolve ip to the name server", err)
-	}
-
-	connection, err := net.ListenUDP("udp", serverAddr)
-	if err != nil {
-		log.Fatal("listen failed", err)
+		log.Fatal(err)
 	}
 	defer connection.Close()
-	log.Printf("listening on <<>> @%s:%s\n\n", DEFAULT_LOCAL_NAME_SERVER, strconv.Itoa(*port))
+	log.Printf("listening on <<>> @%s:%s\n", *ipAddress, strconv.Itoa(*port))
 
 	message := make([]byte, 1024)
 	for {
-		n, clientAddr, err := connection.ReadFromUDP(message)
+		n, clientAddress, err := connection.ReadFromUDP(message)
 		if err != nil {
-			log.Printf("read error: %v", err)
+			log.Printf("read error: %v\n", err)
 			continue
 		}
-
-		var query forwarder.Message
-		query.ParseQueryMessage(message[:n])
-		if debug != nil && *debug {
-			fmt.Printf("%s", query.Print())
+		req := message[:n]
+		if *debug {
+			log.Println("request message")
+			fmt.Printf("%s", hex.Dump(req))
 		}
-		for _, q := range query.Question {
-			log.Printf("query[%s] %s from <<>> %s\n\n", q.GetType(), q.GetName(), clientAddr)
+		res := forwarder.HandleRequest(req, c, clientAddress)
+		if *debug {
+			log.Println("response message")
+			fmt.Printf("%s", hex.Dump(res))
 		}
-
-		request := query.BuildQuery()
-		hex_req, err := hex.DecodeString(request)
+		_, err = connection.WriteToUDP(res, clientAddress)
 		if err != nil {
-			log.Fatal(err)
-		}
-		for _, q := range query.Question {
-			log.Printf("forwarded %s to <<>> %s:%s\n\n", q.GetName(), forwarder.GOOGLE_DNS_SERVER, forwarder.DEFAULT_PORT)
-		}
-
-		res, err := forwarder.SendRequestTo(hex_req, forwarder.GOOGLE_DNS_SERVER)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if !query.ValidateResponse(res) {
-			log.Fatal("invalid response")
-		}
-
-		var response forwarder.Message
-		response.ParseQueryMessage(res)
-		if debug != nil && *debug {
-			fmt.Printf("%s", response.Print())
-		}
-		for _, r := range response.Answer {
-			log.Printf("response[%s] %s is %s\n\n", r.GetType(), r.Name, r.Data)
-		}
-
-		_, err = connection.WriteToUDP(res, clientAddr)
-		if err != nil {
-			log.Printf("write error: %v", err)
+			log.Printf("write error: %v\n", err)
+			continue
 		}
 	}
 }
